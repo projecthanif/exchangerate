6e29c679046f1e796d957c9c0330edd45e4dd56a
projecthanif <iamustapha213@gmail.com>
2024-12-14 23:59:11 +0100

    testing feature

---
+++ src/ExchangeRateServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace Projecthanif\ExchangeRate;
+
+use Illuminate\Support\ServiceProvider;
+
+class ExchangeRateServiceProvider extends ServiceProvider
+{
+
+    public function boot(): void
+    {
+        $this->publishes([
+            __DIR__ . '/../config/projecthanif-exchangerate.php' => config_path('projecthanif-exchangerate.php'),
+        ]);
+    }
+}
